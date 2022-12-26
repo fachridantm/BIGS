f8d50e9712e2bcaa05fcca9de96fef7bf1c7bcd7
fachridantm <fachridantm@gmail.com>
2022-12-26 23:07:58 +0700

    move into package

--- Android Development/app/src/main/java/id/belitong/bigs/main/MainActivity.kt
+++ Android Development/app/src/main/java/id/belitong/bigs/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package id.belitong.bigs
+package id.belitong.bigs.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.belitong.bigs.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
