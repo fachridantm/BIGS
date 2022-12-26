e827effab9a0d1e23ae5daea3e2179d4c7361f15
fachridantm <fachridantm@gmail.com>
2022-12-27 00:29:21 +0700

    create auth activity and fragment

---
+++ Android Development/app/src/main/java/id/belitong/bigs/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package id.belitong.bigs.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.belitong.bigs.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
