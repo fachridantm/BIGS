7bcb88f7ac5e31339866647fce1a624eb9fbd79e
fachridantm <fachridantm@gmail.com>
2022-12-26 23:13:56 +0700

    create screen activity

---
+++ Android Development/app/src/main/java/id/belitong/bigs/splash/ScreenActivity.kt
@@ -0,0 +1,12 @@
+package id.belitong.bigs.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.belitong.bigs.R
+
+class ScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen)
+    }
+}
