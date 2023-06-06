ed710bcd2e7a1751e70942565be7c1602a8bc1a8
fachridantm <fachridantm@gmail.com>
2023-06-06 18:53:04 +0700

    create BaseFragment

---
+++ Android Development/app/src/main/java/id/belitong/bigs/BaseFragment.kt
@@ -0,0 +1,55 @@
+package id.belitong.bigs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
+
+    private var _binding: VB? = null
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = getViewBinding(inflater, container, savedInstanceState)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initIntent()
+        initView()
+        initAction()
+        initProcess()
+        initObservers()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    abstract fun getViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): VB
+
+    abstract fun initIntent()
+
+    abstract fun initView()
+
+    abstract fun initAction()
+
+    abstract fun initProcess()
+
+    abstract fun initObservers()
+
+}
