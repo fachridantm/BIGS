1e84a21fb0ed19384e16a102dd793c58a71cd83c
fachridantm <fachridantm@gmail.com>
2023-06-10 05:12:52 +0700

    implement implements BaseFragment in HomeFragment

---
+++ Android Development/app/src/main/java/id/belitong/bigs/ui/home/HomeFragment.kt
@@ -0,0 +1,40 @@
+package id.belitong.bigs.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import dagger.hilt.android.AndroidEntryPoint
+import id.belitong.bigs.BaseFragment
+import id.belitong.bigs.databinding.FragmentHomeBinding
+
+@AndroidEntryPoint
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+    override fun getViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): FragmentHomeBinding {
+        TODO("Not yet implemented")
+    }
+
+    override fun initIntent() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initAction() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initProcess() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initObservers() {
+        TODO("Not yet implemented")
+    }
+
+}
