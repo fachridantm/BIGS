3eaa14437f64515200daaebdb40c0d6d924724b9
fachridantm <fachridantm@gmail.com>
2023-06-11 19:43:51 +0700

    create ProfileActivity

---
+++ Android Development/app/src/main/java/id/belitong/bigs/ui/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package id.belitong.bigs.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.belitong.bigs.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
