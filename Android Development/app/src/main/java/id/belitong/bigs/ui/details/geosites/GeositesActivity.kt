17ed2f28ec66a46f260ef07e75dac365166ee930
fachridantm <fachridantm@gmail.com>
2023-06-11 23:44:24 +0700

    create geosite detail

---
+++ Android Development/app/src/main/java/id/belitong/bigs/ui/details/geosites/GeositesActivity.kt
@@ -0,0 +1,12 @@
+package id.belitong.bigs.ui.details.geosites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.belitong.bigs.R
+
+class GeositesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_geosites)
+    }
+}
