635c142df183bb55cb6e0b9fdc05f388786aaeba
fachridantm <fachridantm@gmail.com>
2023-06-12 13:33:17 +0700

    create initAction() and initView() with implementing viewBinding

--- Android Development/app/src/main/java/id/belitong/bigs/ui/splash/ScreenActivity.kt
+++ Android Development/app/src/main/java/id/belitong/bigs/ui/splash/ScreenActivity.kt
@@ -1,14 +1,47 @@
 package id.belitong.bigs.ui.splash
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
-import id.belitong.bigs.R
+import id.belitong.bigs.databinding.ActivityScreenBinding
+import id.belitong.bigs.ui.auth.AuthActivity
 
 @AndroidEntryPoint
 class ScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_screen)
+        binding = ActivityScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+        initAction()
+    }
+
+    private fun initView() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.apply {
+                hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+
+    private fun initAction() {
+        binding.btnExplore.setOnClickListener {
+            startActivity(Intent(this, AuthActivity::class.java))
+            finish()
+        }
     }
 }
