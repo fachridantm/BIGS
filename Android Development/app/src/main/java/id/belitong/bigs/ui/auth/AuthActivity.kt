124cea91c51900f17bd40eacc5381c3aa4141a80
fachridantm <fachridantm@gmail.com>
2023-06-12 12:52:39 +0700

    implement view binding

--- Android Development/app/src/main/java/id/belitong/bigs/ui/auth/AuthActivity.kt
+++ Android Development/app/src/main/java/id/belitong/bigs/ui/auth/AuthActivity.kt
@@ -1,14 +1,17 @@
 package id.belitong.bigs.ui.auth
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
-import id.belitong.bigs.R
+import id.belitong.bigs.databinding.ActivityAuthBinding
 
 @AndroidEntryPoint
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- Android Development/app/src/main/java/id/belitong/bigs/ui/main/MainActivity.kt
+++ Android Development/app/src/main/java/id/belitong/bigs/ui/main/MainActivity.kt
@@ -1,14 +1,17 @@
 package id.belitong.bigs.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
-import id.belitong.bigs.R
+import id.belitong.bigs.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
