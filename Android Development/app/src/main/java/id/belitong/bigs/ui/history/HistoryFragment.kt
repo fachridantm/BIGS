6f62f0301267180c88867faeb17cb96d8b99d8fc
fachridantm <fachridantm@gmail.com>
2023-06-18 01:23:11 +0700

    init HistoryFragment

--- Android Development/app/src/main/java/id/belitong/bigs/ui/history/HistoryFragment.kt
+++ Android Development/app/src/main/java/id/belitong/bigs/ui/history/HistoryFragment.kt
@@ -1,4 +1,31 @@
 package id.belitong.bigs.ui.history
 
-class HistoryFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import id.belitong.bigs.BaseFragment
+import id.belitong.bigs.databinding.FragmentHistoryBinding
+
+class HistoryFragment: BaseFragment<FragmentHistoryBinding>() {
+    override fun getViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): FragmentHistoryBinding = FragmentHistoryBinding.inflate(inflater, container, false)
+
+    override fun initData() {
+
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun initAction() {
+
+    }
+
+    override fun initObservers() {
+
+    }
 }
