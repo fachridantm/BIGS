5e54e4d9d07d837ddb8e08a5d3bcf1a8754c4118
fachridantm <fachridantm@gmail.com>
2023-06-18 01:35:39 +0700

    create AddFragment

--- Android Development/app/src/main/java/id/belitong/bigs/ui/add/AddFragment.kt
+++ Android Development/app/src/main/java/id/belitong/bigs/ui/add/AddFragment.kt
@@ -1,4 +1,33 @@
 package id.belitong.bigs.ui.add
 
-class AddFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import dagger.hilt.android.AndroidEntryPoint
+import id.belitong.bigs.BaseFragment
+import id.belitong.bigs.databinding.FragmentAddBinding
+
+@AndroidEntryPoint
+class AddFragment : BaseFragment<FragmentAddBinding>() {
+    override fun getViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): FragmentAddBinding = FragmentAddBinding.inflate(inflater, container, false)
+
+    override fun initData() {
+
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun initAction() {
+
+    }
+
+    override fun initObservers() {
+
+    }
 }
