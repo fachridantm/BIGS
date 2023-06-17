4dda54fbd1cc2fb559b0b515dcae00227d6232dc
fachridantm <fachridantm@gmail.com>
2023-06-18 01:37:52 +0700

    fixing bottom navigation

--- Android Development/app/src/main/java/id/belitong/bigs/ui/main/MainActivity.kt
+++ Android Development/app/src/main/java/id/belitong/bigs/ui/main/MainActivity.kt
@@ -2,16 +2,25 @@ package id.belitong.bigs.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
+import id.belitong.bigs.R
 import id.belitong.bigs.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navView.setupWithNavController(navController)
     }
 }
