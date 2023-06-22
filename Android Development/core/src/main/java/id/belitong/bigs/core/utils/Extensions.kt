893ae1b147ea841ad9f7322f8da19da672e0bb18
fachridantm <fachridantm@gmail.com>
2023-06-22 17:44:09 +0700

    fix placeholder and fun m to km

--- Android Development/core/src/main/java/id/belitong/bigs/core/utils/Extensions.kt
+++ Android Development/core/src/main/java/id/belitong/bigs/core/utils/Extensions.kt
@@ -61,7 +61,7 @@ fun ImageView.loadUserImage(url: String) {
 }
 
 fun Int.meterToKilometer(): String {
-    return if (this > 1000) {
+    return if (this >= 1000) {
         val doubleValue = this.toDouble()
         val distance = doubleValue / 1000
         if (distance % 1 == 0.0) {
