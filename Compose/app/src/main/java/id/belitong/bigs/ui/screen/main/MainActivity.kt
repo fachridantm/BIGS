7ef2f1bbb5a393c8cb261554650db97501c54ca8
fachridantm <fachridantm@gmail.com>
2023-06-26 01:07:51 +0700

    implement main screen pattern

--- Compose/app/src/main/java/id/belitong/bigs/ui/screen/main/MainActivity.kt
+++ Compose/app/src/main/java/id/belitong/bigs/ui/screen/main/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
-import id.belitong.bigs.ui.screen.splash.SplashScreen
 import id.belitong.bigs.ui.theme.BIGSComposeTheme
 
 @AndroidEntryPoint
@@ -22,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SplashScreen()
+                    MainScreen()
                 }
             }
         }
--- Compose/app/src/main/java/id/belitong/bigs/ui/screen/main/MainScreen.kt
+++ Compose/app/src/main/java/id/belitong/bigs/ui/screen/main/MainScreen.kt
@@ -1,11 +1,35 @@
 package id.belitong.bigs.ui.screen.main
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import id.belitong.bigs.ui.navigation.BigsNavHost
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(
     modifier: Modifier = Modifier,
 ) {
+    val navController = rememberNavController()
+    val bottomBarState = rememberSaveable { (mutableStateOf(true)) }
 
+    Scaffold(
+        modifier = modifier,
+        bottomBar = {
+            if (bottomBarState.value) {
+
+            }
+        }
+    ) { innerPadding ->
+        BigsNavHost(
+            modifier = Modifier.padding(innerPadding),
+            navController = navController,
+            isBottomBarVisible = { bottomBarState.value = it }
+        )
+    }
 }
