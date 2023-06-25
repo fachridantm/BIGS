055dcc74549cf9cccdb2f12b5e89057ebbcdb37e
fachridantm <fachridantm@gmail.com>
2023-06-25 23:42:35 +0700

    fix typography

--- Compose/app/src/main/java/id/belitong/bigs/ui/components/Button.kt
+++ Compose/app/src/main/java/id/belitong/bigs/ui/components/Button.kt
@@ -2,11 +2,11 @@ package id.belitong.bigs.ui.components
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,11 +14,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import id.belitong.bigs.R
 import id.belitong.bigs.ui.theme.md_theme_dark_secondary
-import id.belitong.bigs.ui.theme.textSemiboldTitleCenter
+import id.belitong.bigs.ui.theme.typography
 
 @Composable
 fun ButtonWithDrawableEnd(
@@ -41,7 +42,8 @@ fun ButtonWithDrawableEnd(
             Text(
                 modifier = Modifier.padding(horizontal = 32.dp),
                 text = textButton,
-                style = textSemiboldTitleCenter,
+                style = typography.button,
+                textAlign = TextAlign.Center,
                 color = textColor,
             )
             Icon(
--- Compose/app/src/main/java/id/belitong/bigs/ui/theme/Type.kt
+++ Compose/app/src/main/java/id/belitong/bigs/ui/theme/Type.kt
@@ -1,34 +1,81 @@
 package id.belitong.bigs.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import id.belitong.bigs.R
+
+private val Poppins = FontFamily(
+    Font(R.font.poppins_regular),
+    Font(R.font.poppins_medium),
+    Font(R.font.poppins_semi_bold),
+    Font(R.font.poppins_bold),
+)
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+val typography = Typography(
+    defaultFontFamily = Poppins,
+    h1 = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
+    ),
+    h2 = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+    ),
+    h3 = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+    ),
+    h4 = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+    ),
+    h5 = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+    ),
+    body1 = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+    ),
+    body2 = TextStyle(
+        fontFamily = Poppins,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    subtitle1 = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = Poppins,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontSize = 14.sp,
+    ),
+    button = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+    ),
+    caption = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+    ),
+    overline = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+    ),
 )
