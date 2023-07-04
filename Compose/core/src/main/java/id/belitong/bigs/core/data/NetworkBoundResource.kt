e73e77f9d33c68d8dfc5f262e98fa60ded7dda26
fachridantm <fachridantm@gmail.com>
2023-07-04 23:01:06 +0700

    fix state handler

--- Compose/core/src/main/java/id/belitong/bigs/core/data/NetworkBoundResource.kt
+++ Compose/core/src/main/java/id/belitong/bigs/core/data/NetworkBoundResource.kt
@@ -1,29 +1,47 @@
 package id.belitong.bigs.core.data
 
 import id.belitong.bigs.core.data.source.remote.network.ApiResponse
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
-abstract class NetworkBoundResource<ResultType, RequestType> {
+abstract class NetworkBoundResource<ResultType : Any, RequestType> {
     private var result: Flow<Resource<ResultType>> = flow {
-        emit(Resource.Loading())
+        emit(Resource.Loading)
         val dbSource = loadFromDB().first()
         if (shouldFetch(dbSource)) {
-            emit(Resource.Loading())
+            emit(Resource.Loading)
             when (val apiResponse = createCall().first()) {
                 is ApiResponse.Success -> {
                     saveCallResult(apiResponse.data)
-                    emitAll(loadFromDB().map { Resource.Success(it) })
+                    emitAll(
+                        loadFromDB().map {
+                            Resource.Success(it)
+                        }
+                    )
                 }
+
                 is ApiResponse.Empty -> {
-                    emitAll(loadFromDB().map { Resource.Success(it) })
+                    emitAll(
+                        loadFromDB().map {
+                            Resource.Success(it)
+                        }
+                    )
                 }
+
                 is ApiResponse.Error -> {
                     onFetchFailed()
                     emit(Resource.Error(apiResponse.errorMessage))
                 }
             }
         } else {
-            emitAll(loadFromDB().map { Resource.Success(it) })
+            emitAll(
+                loadFromDB().map {
+                    Resource.Success(it)
+                }
+            )
         }
     }
 
--- Compose/core/src/main/java/id/belitong/bigs/core/data/Resource.kt
+++ Compose/core/src/main/java/id/belitong/bigs/core/data/Resource.kt
@@ -1,7 +1,7 @@
 package id.belitong.bigs.core.data
 
-sealed class Resource<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T) : Resource<T>(data)
-    class Loading<T>(data: T? = null) : Resource<T>(data)
-    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+sealed class Resource<out T: Any?> {
+    object Loading : Resource<Nothing>()
+    data class Success<out T: Any>(val data: T) : Resource<T>()
+    data class Error(val errorMessage: String) : Resource<Nothing>()
 }
