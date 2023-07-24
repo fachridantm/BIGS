1fee75a55c20a09367c23f3012c54ac3590c16c6
fachridantm <fachridantm@gmail.com>
2023-07-25 00:10:55 +0700

    create dimension style

---
+++ Compose/app/src/main/java/id/belitong/bigs/ui/theme/Dimension.kt
@@ -0,0 +1,37 @@
+package id.belitong.bigs.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+object Dimension {
+    val SIZE_0 = 0.dp
+    val SIZE_1 = 1.dp
+    val SIZE_2 = 2.dp
+    val SIZE_4 = 4.dp
+    val SIZE_8 = 8.dp
+    val SIZE_10 = 10.dp
+    val SIZE_12 = 12.dp
+    val SIZE_14 = 14.dp
+    val SIZE_16 = 16.dp
+    val SIZE_18 = 18.dp
+    val SIZE_24 = 24.dp
+    val SIZE_32 = 32.dp
+    val SIZE_40 = 40.dp
+    val SIZE_50 = 50.dp
+}
+
+object TextSize {
+    val SIZE_1 = 1.sp
+    val SIZE_2 = 2.sp
+    val SIZE_4 = 4.sp
+    val SIZE_8 = 8.sp
+    val SIZE_10 = 10.sp
+    val SIZE_12 = 12.sp
+    val SIZE_14 = 14.sp
+    val SIZE_16 = 16.sp
+    val SIZE_18 = 18.sp
+    val SIZE_24 = 24.sp
+    val SIZE_32 = 32.sp
+    val SIZE_40 = 40.sp
+    val SIZE_50 = 50.sp
+}
