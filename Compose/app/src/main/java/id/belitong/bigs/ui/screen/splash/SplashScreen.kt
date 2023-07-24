d356fa0eb3cbae4b89fd22982a6b0f3d7fa7efcb
fachridantm <fachridantm@gmail.com>
2023-07-24 23:39:31 +0700

    fix SplashScreen

--- Compose/app/src/main/java/id/belitong/bigs/ui/screen/splash/SplashScreen.kt
+++ Compose/app/src/main/java/id/belitong/bigs/ui/screen/splash/SplashScreen.kt
@@ -1,5 +1,6 @@
 package id.belitong.bigs.ui.screen.splash
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,29 +22,55 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
 import id.belitong.bigs.R
-import id.belitong.bigs.ui.components.ButtonWithDrawableEnd
-import id.belitong.bigs.ui.components.HideSystemUIBars
+import id.belitong.bigs.ui.composable.components.ButtonWithDrawableEnd
+import id.belitong.bigs.ui.composable.components.HideSystemUIBars
+import id.belitong.bigs.ui.composable.utils.getActivity
+import id.belitong.bigs.ui.screen.auth.AuthActivity
+import id.belitong.bigs.ui.screen.auth.login.LoginViewModel
+import id.belitong.bigs.ui.screen.main.MainActivity
 import id.belitong.bigs.ui.theme.md_theme_dark_secondary
 import id.belitong.bigs.ui.theme.typography
 
 @Composable
+@Destination
+@RootNavGraph(true)
 fun SplashScreen(
-    modifier: Modifier = Modifier,
-    onClickToNavigate: () -> Unit,
+    loginViewModel: LoginViewModel = hiltViewModel(),
 ) {
+
+    val activity = getActivity()
+
+    val getTokenState = loginViewModel.getToken().observeAsState()
+
     HideSystemUIBars()
 
+    BackHandler {
+        activity.finish()
+    }
+
     SplashScreenContent(
-        modifier = modifier,
-        onClickToNavigate = onClickToNavigate,
+        onClick = {
+            getTokenState.value?.let { token ->
+                if (token.isNotEmpty()) {
+                    MainActivity.start(activity)
+                    activity.finish()
+                } else {
+                    AuthActivity.start(activity)
+                    activity.finish()
+                }
+            }
+        }
     )
 }
 
 @Composable
 fun SplashScreenContent(
     modifier: Modifier = Modifier,
-    onClickToNavigate: () -> Unit,
+    onClick: () -> Unit,
 ) {
     Box(
         modifier = modifier.fillMaxSize()
@@ -91,7 +119,7 @@ fun SplashScreenContent(
                 textColor = Color.Black,
                 drawableEnd = painterResource(R.drawable.ic_explore_now),
                 drawableTint = Color.Black,
-                onClick = onClickToNavigate,
+                onClick = onClick,
             )
         }
     }
@@ -101,6 +129,6 @@ fun SplashScreenContent(
 @Composable
 fun SplashScreenPreview() {
     SplashScreenContent(
-        onClickToNavigate = {}
+        onClick = {}
     )
 }
