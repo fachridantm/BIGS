65163d0d283f317ab3a23f4858d9af47a7900500
fachridantm <fachridantm@gmail.com>
2023-07-24 23:38:14 +0700

    add fun start to navigate

--- Compose/app/src/main/java/id/belitong/bigs/ui/screen/main/MainActivity.kt
+++ Compose/app/src/main/java/id/belitong/bigs/ui/screen/main/MainActivity.kt
@@ -1,14 +1,12 @@
 package id.belitong.bigs.ui.screen.main
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
 import id.belitong.bigs.ui.theme.BIGSComposeTheme
-import id.belitong.bigs.ui.theme.md_theme_light_background
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -16,12 +14,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BIGSComposeTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = md_theme_light_background
-                ) {
-                    MainScreen()
-                }
+                MainScreen()
+            }
+        }
+    }
+
+    companion object {
+        fun start(context: Context) {
+            Intent(context, MainActivity::class.java).apply {
+                context.startActivity(this)
             }
         }
     }
