c7edc6eb53972d0084138eeae635482bc849439d
fachridantm <fachridantm@gmail.com>
2023-08-01 16:32:24 +0700

    create AddScreen and HistoryScreen

---
+++ Compose/app/src/main/java/id/belitong/bigs/ui/screen/history/HistoryScreen.kt
@@ -0,0 +1,15 @@
+package id.belitong.bigs.ui.screen.history
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import id.belitong.bigs.ui.navigation.MainNavGraph
+
+@MainNavGraph
+@Destination
+@Composable
+fun HistoryScreen(
+    navigator: DestinationsNavigator? = null
+) {
+    // TODO 5: Create UI HistoryScreen
+}
