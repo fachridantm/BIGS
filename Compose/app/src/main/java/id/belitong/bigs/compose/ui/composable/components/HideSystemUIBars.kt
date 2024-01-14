a118a975b1757e04a154210df35fb9ae623478c1
fachridantm <fachridantm@gmail.com>
2024-01-14 16:29:41 +0700

    feat(main): change deprecated object

--- Compose/app/src/main/java/id/belitong/bigs/compose/ui/composable/components/HideSystemUIBars.kt
+++ Compose/app/src/main/java/id/belitong/bigs/compose/ui/composable/components/HideSystemUIBars.kt
@@ -2,14 +2,15 @@ package id.belitong.bigs.compose.ui.composable.components
 
 import android.app.Activity
 import android.os.Build
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowInsetsController
-import android.view.WindowManager
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.view.WindowCompat
 
+@Suppress("DEPRECATION")
 @Composable
 fun HideSystemUIBars() {
     val context = LocalContext.current
@@ -17,20 +18,19 @@ fun HideSystemUIBars() {
     DisposableEffect(Unit) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             val window = (context as? Activity)?.window
-            val windowInsetsController = window?.insetsController
-            windowInsetsController?.apply {
-                hide(
-                    WindowInsets.Type.statusBars()
-                            or WindowInsets.Type.navigationBars()
-                )
-                systemBarsBehavior =
-                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            val controller = window?.insetsController
+            controller?.apply {
+                hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             }
         } else {
-            (context as? Activity)?.window?.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
+            val window = (context as? Activity)?.window
+            window?.decorView?.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
         }
 
         onDispose {
